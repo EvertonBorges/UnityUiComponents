81350b13396a15790f650dc9460c23d1c1846315
EvertonBorges <evertonsoaresborges@gmail.com>
2020-10-07 23:46:17 -0400

    ScaleEffect

---
+++ Assets/Scripts/UiScaleEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[
+    RequireComponent(typeof(Graphics))
+]
+public class UiScaleEffect : MonoBehaviour
+{
+
+    [Range(0.1f, 5f)] [SerializeField] private float animationTime = 1f;
+    private RectTransform _rectTransform;
+
+    private Coroutine _currentCoroutine = null;
+
+    void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _rectTransform.localScale = Vector2.zero;
+    }
+
+    public void ScaleOut() 
+    {
+        if (_currentCoroutine != null) StopCoroutine(_currentCoroutine);
+        _currentCoroutine = StartCoroutine(Scale(true));
+    }
+
+    public void ScaleIn() {
+        if (_currentCoroutine != null) StopCoroutine(_currentCoroutine);
+        _currentCoroutine = StartCoroutine(Scale(false));
+    } 
+
+    private IEnumerator Scale(bool normalDirection)
+    {
+        Vector2 startScale = normalDirection ? Vector2.zero : Vector2.one;
+        Vector2 endScale = normalDirection ? Vector2.one : Vector2.zero;
+
+        float startProportion = (float) _rectTransform.localScale.x / animationTime;
+        float currentTime = normalDirection ? startProportion : (2f - startProportion);
+
+        while(currentTime <= animationTime)
+        {
+            currentTime += Time.deltaTime;
+            float timeProportion = currentTime / animationTime;
+            Vector2 scale = Vector2.Lerp(startScale, endScale, timeProportion);
+            _rectTransform.localScale = scale;
+            yield return null;
+        }
+
+        _rectTransform.localScale = endScale;
+        yield return null;
+
+        _currentCoroutine = null;
+    }
+
+}
